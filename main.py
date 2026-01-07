e2594f03b85bef767d29782a524cf4b2546001b6
trimpbizkit <mgilmour.se@gmail.com>
2026-01-06 23:32:05 -0500

    add and draw player triangle

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
 
     # infinite loop to draw the game to the screen
     while True:
@@ -18,6 +22,8 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
+
         pygame.display.flip()
         dt += clock.tick(60) / 1000
 
